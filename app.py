2d474f2e076a16ecd37260c394c503c48a73d8fa
akshit22sept <akshit22sept@gmail.com>
2021-02-25 11:33:40 +0530

    good looks

--- app.py
+++ app.py
@@ -12,9 +12,12 @@ def index():
         h1 {
       background-color: #d5f4e6;
     }
+    h2 {
+      background-color: #d5f4e6;
+    }
     </style>
     <font face = "Lucida Calligraphy" size =" 5" color="orange"><b><h1 align="center">Hello SyntaEXE Team !</h1><b></font>
-    <p1>Good luck team and remember, the members in the server are always ready to help !</p1>
+    <font face = "Lucida Calligraphy" color="orange"><h2>Good luck team and remember, the members in the server are always ready to help !</h2></font>
     '''
 
 
