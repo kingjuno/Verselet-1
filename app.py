8f7fc25406f62bf8c898d26d9a934b59ce227038
mordy-python <60708054+mordy-python@users.noreply.github.com>
2021-02-25 11:12:58 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, get_flashed_messages
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 import pandas as pd
+
 app = Flask(__name__)
-app.secret_key = 'hi'
+# u need a much stronger secret key so here it is
+app.secret_key = 'ec52e5ead3899e4a0717b9806e1125de8af3bad84ca7f511'
 
 class User:
     def __init__(self, id, username, password):
@@ -11,17 +13,19 @@ class User:
 
 @app.route('/')
 def front():
-    return render_template('front.html')
-
-
-@app.route('/login', methods=['GET', 'POST'])
+    if 'user' in session:
+        return render_template('front.html', name=session['user'])
+    else:
+        return render_template('front.html')
+@app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == "POST":
         df = pd.read_csv('user.csv')
         user = request.form.get("uname")
         password = request.form.get("psw")
-        if df[df['User'] == user]['Pass'].values == password:
+        if df[df['User']==user]['Pass'].values==password:
             flash(f'you are logged in {user}')
+            session['user'] = user
             return redirect(url_for('front'))
         else:
             flash('wrong username password')
@@ -29,7 +33,7 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/register', methods=['POST', 'GET'])
+@app.route('/register',methods=['POST','GET'])
 def register():
     if request.method == "POST":
         df = pd.read_csv('user.csv')
@@ -37,7 +41,7 @@ def register():
         user = request.form.get("uname")
         password = request.form.get("psw")
         if user in df.User.values:
-            flash(f'Username {user} is already took')
+            flash(f'Username {user} is already taken')
             return redirect(url_for('register'))
         else:
             df = df.append({'User': user, 'Pass': password, 'Email': email, 'Id': len(df) + 1}, ignore_index=True)
