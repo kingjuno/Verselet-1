4a4e03bcc20047af94b4b3a0ed8020fb711832ef
akshit22sept <akshit22sept@gmail.com>
2021-02-25 11:02:52 +0530

    Update app.py
    hello SyntaEXE

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# Good luck team !!
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +7,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return '''
+    <style>
+        h1 {
+      background-color: #d5f4e6;
+    }
+    </style>
+    <font face = "Lucida Calligraphy" size =" 5" color="orange"><b><h1 align="center">Hello SyntaEXE Team</h1><b></font>'''
 
 
 if __name__ == '__main__':
