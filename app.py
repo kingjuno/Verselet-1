c08f9cf14d8bf019cde5c9f8d22b2785a6628e4b
Geo Jolly <69108486+kingjuno@users.noreply.github.com>
2021-02-25 22:56:50 +0530

    Update app.py
    Added 404 page

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ def front():
         return render_template('front.html', name=session['user'])
     else:
         return render_template('front.html')
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+
 @app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == "POST":
