0f5a6e74008706fdf9f1b646be73fe7d5e578dad
akshit22sept <akshit22sept@gmail.com>
2021-02-26 11:16:19 +0530

    sessions and profile page

--- app.py
+++ app.py
@@ -28,13 +28,12 @@ def front():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    session.clear()
     if request.method == "POST":
         df = pd.read_csv('user.csv')
         user = request.form.get("uname")
         password = request.form.get("psw")
         if decoder(df[df['User'] == user]['Pass'].values[0]) == password:
-            flash(f'Welcome, {user}')
-            session['user'] = user
             return redirect(url_for('user_profile'))
         else:
             flash('Incorrect username or password')
@@ -53,7 +52,10 @@ def register():
             flash(f'Username {user} is already taken')
             return redirect(url_for('register'))
         else:
+            df2=pd.read_csv('db.csv')
             df = df.append({'User': user, 'Pass': password, 'Email': email, 'Id': len(df) + 1}, ignore_index=True)
+            df2 = df2.append({'Wins':0,'Games':0,'Avr. Time':0,'Username':user},ignore_index=True)
+            df2.to_csv('db.csv',index=False)
             df.to_csv('user.csv', index=False)
             return redirect(url_for('front'))
     return render_template('register.html')
@@ -61,7 +63,14 @@ def register():
 
 @app.route('/profile')
 def user_profile():
-    return render_template('profile.html')
+    try:
+        df2=pd.read_csv('db.csv')
+        wins = df2[df2['Username'] == session['user']]['Wins'].values[0]
+        games = df2[df2['Username'] == session['user']]['Games'].values[0]
+        return render_template('profile.html',w=wins,g=games,u=session['user'])
+    except:
+       flash('Login First')
+       return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
