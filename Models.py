7d793ddc13baab7c138cdb72d1332ee887876434
Emil <eb531@web.de>
2021-03-21 17:14:12 +0100

    Added db for Nuke Ninja

--- Models.py
+++ Models.py
@@ -2,9 +2,40 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Room(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    link = db.Column(db.String(8), unique=True, nullable=False) # the part of the link after rooms/
-    users= db.Column(db.String(80), nullable=False) #list of users in room converted to string
-
+    id     = db.Column(db.Integer  , primary_key=True)
+    link   = db.Column(db.String(8), unique=True, nullable=False) # the part of the link after rooms/
+    status = db.Column(db.String )
+    names  = db.Column(db.String)
+    code= db.Column(db.String)
     def __repr__(self):
         return f"<Room {self.id}:{self.link}>"
+def init_room(link,status,names,code):
+    room= Room(link=link,status=status,names=names,code=code)
+    db.session.add(room)
+    db.session.commit()
+    return room.id
+
+def delete_room(link):
+    room=Room.query.filter_by(link=link).first()
+    db.session.delete(room)
+    db.session.commit()
+
+
+def get_room(link):
+    room=Room.query.filter_by(link=link).first()
+    if room is None:
+        return "NO SUCH ROOM"
+    return {"link":room.link, "status":room.status,"names":room.names,"code":room.code}
+def update_room(link, status="",names="",code=""):
+    """
+    link is required to identify the room 
+    insert new values for current room can change anything besides the link
+    e.g to change only the names write update_room("somelink",names="some new names")
+    """
+    room = Room.query.filter_by(link=link).first()
+    if status != "":
+        room.status=status
+    if names != "":
+        room.names=names
+    if code != "":
+        room.code=code
